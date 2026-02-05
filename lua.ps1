eaad07ad6bdffcf90f8b9cfedfc582c42f22db67
[LuaTools] Ookami <95390117+ookami42@users.noreply.github.com>
2026-02-05 02:57:05 -0300

    Update lua.ps1
    corrigido caminho para última versão

--- lua.ps1
+++ lua.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-$uri = "https://github.com/ookami42/lua-install/releases/latest/download/easy-setup.ps1"
+$uri = "https://github.com/ookami42/ookami42.github.io/releases/latest/download/easy-setup.ps1"
 
 Write-Host "Downloading installer..."
 irm $uri | iex
