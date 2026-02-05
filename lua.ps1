e9b27e6d29539d05c0d6d98f3c3a957a7338d62c
[LuaTools] Ookami <95390117+ookami42@users.noreply.github.com>
2026-02-05 13:56:56 -0300

    Delete lua.ps1
    apagada a gambiarra

--- lua.ps1
+++
@@ -1,6 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$uri = "https://github.com/ookami42/ookami42.github.io/releases/latest/download/easy-setup.ps1"
-
-Write-Host "Downloading installer..."
-irm $uri | iex
